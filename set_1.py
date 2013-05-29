76939afc13fdd1a5d3f61c90e55f049da97af2b2
William Ehlhardt <williamehlhardt@gmail.com>
2013-05-29 01:06:23 -0500

    frequencies

--- set_1.py
+++ set_1.py
@@ -272,6 +272,44 @@ def english_words_metric(vec):
     score = sum(len(w) for w in words_found)
     return score / float(len(vec))
 
+# Frequency table cribbed from
+# http://en.algoritmy.net/article/40379/Letter-frequency-English
+ENGLISH_LETTER_FREQUENCIES=[
+    ['A', '8.167'],
+    ['B', '1.492'],
+    ['C', '2.782'],
+    ['D', '4.253'],
+    ['E', '12.702'],
+    ['F', '2.228'],
+    ['G', '2.015'],
+    ['H', '6.094'],
+    ['I', '6.966'],
+    ['J', '0.153'],
+    ['K', '0.772'],
+    ['L', '4.025'],
+    ['M', '2.406'],
+    ['N', '6.749'],
+    ['O', '7.507'],
+    ['P', '1.929'],
+    ['Q', '0.095'],
+    ['R', '5.987'],
+    ['S', '6.327'],
+    ['T', '9.056'],
+    ['U', '2.758'],
+    ['V', '0.978'],
+    ['W', '2.360'],
+    ['X', '0.150'],
+    ['Y', '1.974'],
+    ['Z', '0.074']
+    ]
+ENGLISH_LETTER_FREQUENCIES = dict(
+    letter, float(val)/100 for letter,val in ENGLISH_LETTER_FREQUENCIES)
+
+def english_letters_metric(vec):
+    """Compute how likely this particular frequency distribution of
+    letters is using a multinomial distribution over English letters.
+    """
+    
 
 def crack_xorchar(vec):
     """Attempts to crack the xorchar "encryption" applied to byte array 'vec'"""
