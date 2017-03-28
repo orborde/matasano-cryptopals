db33e0627cb77a8eeb49086787027b306dbfc8d8
William Ehlhardt <williamehlhardt@gmail.com>
2017-03-27 23:55:48 -0700

    Add p26 text.

--- set4p26.py
+++ set4p26.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 
+"""There are people in the world that believe that CTR resists bit
+flipping attacks of the kind to which CBC mode is susceptible.
+
+Re-implement the CBC bitflipping exercise from earlier (#16) to use
+CTR mode instead of CBC mode. Inject an "admin=true" token.
+
+"""
+
 import AES128
 
 KEY = AES128.gen_key()
