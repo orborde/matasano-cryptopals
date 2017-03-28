a8bfc26f55a6dd01d8054c448d175d2ee8cb3dec
William Ehlhardt <williamehlhardt@gmail.com>
2017-03-27 23:54:45 -0700

    Rewrite p26 to use CTR

--- set4p26.py
+++ set4p26.py
@@ -1,28 +1,36 @@
 #!/usr/bin/env python3
 
-P16_KEY = os.urandom(AES128.KEYSIZE)
-P16_PREFIX = b'comment1=cooking%20MCs;userdata='
-P16_SUFFIX = b';comment2=%20like%20a%20pound%20of%20bacon'  
-def p16_cookie(userdata):
+import AES128
+
+KEY = AES128.gen_key()
+PREFIX = b'comment1=cooking%20MCs;userdata='
+SUFFIX = b';comment2=%20like%20a%20pound%20of%20bacon'
+
+def cookie(userdata):
     """
-    >>> print(p16_cookie_decode(p16_cookie(b'hello')).decode())
+    >>> print(cookie_decode(cookie(b'hello')).decode())
     comment1=cooking%20MCs;userdata=hello;comment2=%20like%20a%20pound%20of%20bacon
-    >>> print(p16_cookie_decode(p16_cookie(b';admin=true;')).decode())
+    >>> print(cookie_decode(cookie(b';admin=true;')).decode())
     comment1=cooking%20MCs;userdata=\;admin\=true\;;comment2=%20like%20a%20pound%20of%20bacon
     """
     # Sanitize
     userdata = userdata.replace(b';', b'\\;')
     userdata = userdata.replace(b'=', b'\\=')
     # And go!
-    data = (P16_PREFIX + userdata + P16_SUFFIX)
-    return AES128.CBC_encrypt(pkcs7pad(data, AES128.BLOCKSIZE), P16_KEY)
+    nonce = AES128.CTR_gen_nonce()
+    data = (PREFIX + userdata + SUFFIX)
+    ct = AES128.CTR_crypt(KEY, nonce, data)
+    return nonce + ct
 
-def p16_cookie_decode(cookie):
-    return pkcs7unpad(AES128.CBC_decrypt(cookie, P16_KEY))
+def cookie_decode(cookie):
+    split = AES128.CTR_NONCE_SIZE
+    nonce, data = cookie[:split], cookie[split:]
+    return AES128.CTR_crypt(KEY, nonce, data)
 
-def p16_cookie_is_admin(cookie):
-    return b'admin=true' in p16_cookie_decode(cookie)
+def cookie_is_admin(cookie):
+    return b';admin=true;' in cookie_decode(cookie)
 
+import doctest
 def load_tests(loader, tests, ignore):
     tests.addTests(doctest.DocTestSuite())
     return tests
